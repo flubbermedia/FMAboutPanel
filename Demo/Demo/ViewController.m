c2603e3a18bd84164907c178f330023b02476d7b
Andrea Ottolina <3297+sgabello@users.noreply.github.com>
2020-04-19 07:49:39 +0100

    Remove deprecated method

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -21,12 +21,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
 	if (UI_USER_INTERFACE_IDIOM()==UIUserInterfaceIdiomPhone)
